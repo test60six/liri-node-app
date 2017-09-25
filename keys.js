d2d24e710217a83d18947a6cf99953ce2b4638ea
test60six <test60six@gmail.com>
2017-09-25 17:39:58 -0400

    update

--- keys.js
+++ keys.js
@@ -1,19 +1,18 @@
 console.log('this is loaded');
-
 var twitterKeys = {
-    consumer_key: OHvKugM1eR1tExEW85Nl6m8J8,
-    consumer_secret: DIpkeJ4xEkXcbKoynsc3h8PaviErk2PZyUzhcuYeVv9iVATckM,
-    access_token_key: 911953851133186050 - Tz3dcWHmbDCJxKiSzQE6QgvFMSm671a,
-    access_token_secret: FIzTKuuJ9imrZl8peTHRhcb2ubILSenZKrrcnNYLPRqiz,
-}
+    consumer_key: "OHvKugM1eR1tExEW85Nl6m8J8",
+    consumer_secret: "DIpkeJ4xEkXcbKoynsc3h8PaviErk2PZyUzhcuYeVv9iVATckM",
+    access_token_key: "911953851133186050 - Tz3dcWHmbDCJxKiSzQE6QgvFMSm671a",
+    access_token_secret: "FIzTKuuJ9imrZl8peTHRhcb2ubILSenZKrrcnNYLPRqiz",
+};
 
 module.exports = twitterKeys;
 
-var spotify = {
-client_id: e2d3434be125475fbca8c7ce1ffc3b7a
-client_secret: dddceb6244074141a465ea5879a9e4ad
-}
+// var spotify = {
+//     client_id:"e2d3434be125475fbca8c7ce1ffc3b7a",
+//     client_secret: "57d8efbc8e954218bc8a2e3f5d4ab76e"
+// };
 
-var movies = {
-	key:a3c5d16d
-}
+// var movies = {
+//     key: "a3c5d16d"
+// };
--- liri.js
+++ liri.js
@@ -1,17 +1,57 @@
- console.log("test"); 
+ console.log("test");
+ // var data = process.argv[2];
+ // var data1 = process.argv[3];
+ // var data2 = process.argv[4];
+ // var data3 = process.argv[5];
 
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id:e2d3434be125475fbca8c7ce1ffc3b7a,
-  secret:dddceb6244074141a465ea5879a9e4ad
+
+ var fs = require("fs");
+
+ var request = require("request");
+ var keys=require("./keys.js");
+
+ request("http://www.omdbapi.com/?apikey=[a3c5d16d]&r"), function(error, response, body) {
+
+             if (!error && response.statusCode === 200) {
+
+                 console.log(body);
+             }
+         };
+
+         // var SpotifyWebApi = require('spotify-web-api-node');
+
+
+         // var spotifyApi = new SpotifyWebApi({
+         //     clientId: 'e2d3434be125475fbca8c7ce1ffc3b7a',
+         //     clientSecret: '57d8efbc8e954218bc8a2e3f5d4ab76e',
+
+         // });
+
+         // var SpotifyWebApi = require('spotify-web-api-node');
+
+         // //var spotifyApi = new SpotifyWebApi();
+
+         // spotifyApi.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
+         // .then(function(data) {
+         //     console.log('Artist information', data.body);
+         // }, function(err) {
+         //     console.error(err);
+         // });
+
+
+         var Twitter = require('twitter');
+ //console.log(keys);
+var client = new Twitter({
+  consumer_key: keys.consumer_key,
+  consumer_secret:  keys.consumer_secret,
+  access_token_key:  keys.token_key,
+  access_token_secret:  keys.token_secret,
 });
  
-spotify
-  .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets.length);
+    console.log(tweets[0].text);
+  }
+});
